f65a54332a77dbd74950a05a05fae5195aeed5e9
Tom Houlé <tom@tomhoule.com>
2023-10-19 11:15:02 +0200

    wip: swc

--- cli/crates/typed-resolvers/src/type_extensions_from_resolver.rs
+++ cli/crates/typed-resolvers/src/type_extensions_from_resolver.rs
@@ -1,5 +1,128 @@
-use std::path::Path;
+use std::{fmt::Write as _, fs, io, path::Path};
+use swc_common::SourceFile;
+use swc_ecma_ast as ast;
+use swc_ecma_parser as parser;
 
-pub(crate) fn type_extensions_from_resolver(path: &Path, out: &mut String, errs: &mut String) {
-    
+pub(crate) fn object_extension_for_resolver(path: &Path, out: &mut String, errs: &mut String) {
+    let module = match parse_file(path) {
+        Ok(module) => module,
+        Err(err) => {
+            writeln!(errs, "{}", err).ok();
+            return;
+        }
+    };
+    let resolver_fn = module
+        .body
+        .iter()
+        .find_map(|item| match item {
+            ast::ModuleItem::ModuleDecl(ast::ModuleDecl::ExportDefaultExpr(expr)) => {
+                match expr.expr.as_ref() {
+                    ast::Expr::Fn(func) => Some(func),
+                    _ => todo!(), // todo expected export default fn
+                }
+            }
+            _ => None, // todo handle
+        })
+        .unwrap(); // todo handle absence of default export
+
+    let resolver_name = resolver_name(&resolver_fn);
+    let mut resolver_parent = resolver_parent(&resolver_fn);
+    let mut resolver_return_type = resolver_return_type(&resolver_fn);
+
+    writeln!(
+        out,
+        "extend {resolver_parent} {{ {resolver_name}: {resolver_return_type} }}"
+    )
+    .unwrap();
+}
+
+fn resolver_name(func: &ast::FnExpr) -> String {
+    let ident = func.ident.as_ref().expect("function must have a name");
+    ident.to_string()
+}
+
+fn resolver_parent(func: &ast::FnExpr) -> String {
+    let first_param = func.function.params.first().unwrap();
+    match &first_param.pat {
+        ast::Pat::Ident(ast::BindingIdent { id: _, type_ann }) => match type_ann {
+            Some(ann) => match ann.type_ann.as_ref() {
+                ast::TsType::TsKeywordType(_) => todo!(),
+                ast::TsType::TsThisType(_) => todo!(),
+                ast::TsType::TsFnOrConstructorType(_) => todo!(),
+                ast::TsType::TsTypeRef(_) => todo!(),
+                ast::TsType::TsTypeQuery(_) => todo!(),
+                ast::TsType::TsTypeLit(_) => todo!(),
+                ast::TsType::TsArrayType(_) => todo!(),
+                ast::TsType::TsTupleType(_) => todo!(),
+                ast::TsType::TsOptionalType(_) => todo!(),
+                ast::TsType::TsRestType(_) => todo!(),
+                ast::TsType::TsUnionOrIntersectionType(_) => todo!(),
+                ast::TsType::TsConditionalType(_) => todo!(),
+                ast::TsType::TsInferType(_) => todo!(),
+                ast::TsType::TsParenthesizedType(_) => todo!(),
+                ast::TsType::TsTypeOperator(_) => todo!(),
+                ast::TsType::TsIndexedAccessType(_) => todo!(),
+                ast::TsType::TsMappedType(_) => todo!(),
+                ast::TsType::TsLitType(_) => todo!(),
+                ast::TsType::TsTypePredicate(_) => todo!(),
+                ast::TsType::TsImportType(_) => todo!(),
+            },
+            None => todo!(),
+        },
+        _ => todo!(),
+    }
+}
+
+fn resolver_return_type(func: &ast::FnExpr) -> String {
+    match &func.function.return_type {
+        Some(ty) => match ty.type_ann.as_ref() {
+            ast::TsType::TsKeywordType(_) => todo!(),
+            ast::TsType::TsThisType(_) => todo!(),
+            ast::TsType::TsFnOrConstructorType(_) => todo!(),
+            ast::TsType::TsTypeRef(_) => todo!(),
+            ast::TsType::TsTypeQuery(_) => todo!(),
+            ast::TsType::TsTypeLit(_) => todo!(),
+            ast::TsType::TsArrayType(_) => todo!(),
+            ast::TsType::TsTupleType(_) => todo!(),
+            ast::TsType::TsOptionalType(_) => todo!(),
+            ast::TsType::TsRestType(_) => todo!(),
+            ast::TsType::TsUnionOrIntersectionType(_) => todo!(),
+            ast::TsType::TsConditionalType(_) => todo!(),
+            ast::TsType::TsInferType(_) => todo!(),
+            ast::TsType::TsParenthesizedType(_) => todo!(),
+            ast::TsType::TsTypeOperator(_) => todo!(),
+            ast::TsType::TsIndexedAccessType(_) => todo!(),
+            ast::TsType::TsMappedType(_) => todo!(),
+            ast::TsType::TsLitType(_) => todo!(),
+            ast::TsType::TsTypePredicate(_) => todo!(),
+            ast::TsType::TsImportType(_) => todo!(),
+        },
+        None => todo!(),
+    }
+}
+
+fn parse_file(path: &Path) -> io::Result<ast::Module> {
+    let mut recovered_errors = Vec::new(); // not used by us
+    let source_file = path_to_swc_source_file(path)?;
+    let module = parser::parse_file_as_module(
+        &source_file,
+        parser::Syntax::Typescript(parser::TsConfig::default()),
+        ast::EsVersion::EsNext,
+        None,
+        &mut recovered_errors,
+    )
+    .unwrap();
+    Ok(module)
+}
+
+fn path_to_swc_source_file(path: &Path) -> io::Result<SourceFile> {
+    let src = fs::read_to_string(path)?;
+    let file_name = swc_common::FileName::Real(path.to_owned());
+    Ok(SourceFile::new(
+        file_name.clone(),
+        false,
+        file_name,
+        src,
+        swc_common::BytePos::DUMMY,
+    ))
 }
--- cli/crates/typed-resolvers/tests/resolver_discovery.rs
+++ cli/crates/typed-resolvers/tests/resolver_discovery.rs
@@ -9,12 +9,32 @@ fn update_expect() -> bool {
 
 #[allow(clippy::unnecessary_wraps)] // we can't change the signature expected by datatest_stable
 fn run_test(path: &Path) -> datatest_stable::Result<()> {
-    let graphql_schema = fs::read_to_string(path)?;
-    let expected = fs::read_to_string(path.with_file_name("expected.out"))?;
-    let actual = typed_resolvers::generate_type_extensions();
+    let expected_file_path = path.with_file_name("expected.out");
+    let expected = fs::read_to_string(&expected_file_path).unwrap_or_default();
+    let actual = match typed_resolvers::generate_type_extensions_from_resolvers(path) {
+        Ok(graphql) => graphql,
+        Err(errs) => errs,
+    };
 
-    
-    panic!("failed {path:?}");
+    if expected == actual {
+        return Ok(());
+    }
+
+    if update_expect() {
+        std::fs::write(expected_file_path, &actual).unwrap();
+        return Ok(());
+    }
+
+    panic!(
+        "{}\n\n\n=== Hint: run the tests again with UPDATE_EXPECT=1 to update the snapshot. ===",
+        similar::udiff::unified_diff(
+            similar::Algorithm::default(),
+            &expected,
+            &actual,
+            5,
+            Some(("Expected", "Actual"))
+        )
+    );
 }
 
 datatest_stable::harness! { run_test, "./tests/resolver_discovery", r#"^.*\.graphql$"# }
