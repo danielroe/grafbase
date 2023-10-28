b82c53e55b7955473962a0f95db93c8c1e86e36d
Tom Houlé <tom@tomhoule.com>
2023-10-28 11:59:28 +0200

    also build cli-app

---
+++ nix/packages/cli-app.nix
@@ -0,0 +1,18 @@
+{ dream2nix, config, lib, ... }: {
+  imports = [
+    dream2nix.modules.dream2nix.nodejs-package-lock-v3
+    dream2nix.modules.dream2nix.nodejs-granular-v3
+  ];
+
+  name = "cli-app";
+  version = "0.0.1";
+
+  nodejs-package-lock-v3.packageLockFile = "${config.mkDerivation.src}/package-lock.json";
+
+  mkDerivation = {
+    src = ../../packages/cli-app;
+    buildPhase = ''
+      npm run cli-app:build
+    '';
+  };
+}
