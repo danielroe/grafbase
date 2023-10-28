27fedf5aad927c0a5d68c2b673a310d8bf6c6a69
Tom Houlé <tom@tomhoule.com>
2023-10-28 10:35:11 +0200

    wip

--- nix/packages/cli.nix
+++ nix/packages/cli.nix
@@ -1,4 +1,4 @@
-{ dream2nix, config, lib, self, ... }: {
+{ dream2nix, config, lib, ... }: {
   imports = [
     dream2nix.modules.dream2nix.rust-cargo-lock
     dream2nix.modules.dream2nix.rust-crane
@@ -8,11 +8,21 @@
   version = "0.41.2"; # TODO read the actual version.
 
   deps = { nixpkgs, ... }: {
-    GRAFBASE_CLI_ASSETS_GZIP_PATH = nixpkgs.hello;
+    inherit (nixpkgs) hello;
   };
 
   mkDerivation = {
     src = ../../.;
+    preBuild = ''
+      mkdir -p assets/assets
+      mkdir -p assets/static/assets
+      tar czf assets.tgz assets
+
+      mkdir -p pathfinder_bundle/assets
+
+      export GRAFBASE_CLI_ASSETS_GZIP_PATH=`pwd`/assets.tgz
+      export GRAFBASE_CLI_PATHFINDER_BUNDLE_PATH=`pwd`/pathfinder_bundle
+    '';
   };
 
   rust-cargo-lock.source = ../../.;
